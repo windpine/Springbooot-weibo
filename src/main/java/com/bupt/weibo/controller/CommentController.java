e50c2eb6cd04099ee7b20a887dd021915a4b8071
jinffee <jinffee@163.com>
2018-07-14 11:16:39 +0800

    修改commentController,tweetController满足Mapstruct

--- src/main/java/com/bupt/weibo/controller/CommentController.java
+++ src/main/java/com/bupt/weibo/controller/CommentController.java
@@ -5,10 +5,16 @@ import com.alibaba.fastjson.JSONObject;
 import com.bupt.weibo.dto.CommentDTO;
 import com.bupt.weibo.dto.ResultDTO;
 import com.bupt.weibo.entity.Comment;
+import com.bupt.weibo.exception.ResultException;
 import com.bupt.weibo.service.CommentService;
+import com.bupt.weibo.utils.ApplicationUtils;
 import com.bupt.weibo.utils.ResultUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,41 +25,60 @@ import java.util.Map;
  * @time 2018/7/7
  * @description
  */
+//查询无comment，或查询失败抛出异常'no comment'
 @RestController
-@RequestMapping("/comments")
+@RequestMapping(value = CommentController.PATH)
 public class CommentController {
-
+    public static final String PATH = "/comments";
+    public static final String TIDPATH = "/{TID}";
+    public static final String CIDPATH = "/{CID}";
     @Autowired
     CommentService commentService;
-
     //获取某一微博的所有评论
-    @GetMapping("/{TID}")
-    public ResultDTO getTweetComments(@PathVariable(name="TID") Integer TID){
+    @GetMapping(value = TIDPATH)
+    public ResponseEntity<ResultDTO> getTweetComments(UriComponentsBuilder uriComponentsBuilder, @PathVariable(name="TID") Integer TID){
+        //包装header
+        HttpHeaders headers = ApplicationUtils.getHttpHeaders(uriComponentsBuilder,PATH+"/"+TID);
+        headers.setAccessControlAllowCredentials(true);
+        headers.setAccessControlAllowOrigin("*");
+        //map comments
         List<Comment> comments = commentService.getTweetComments(TID);
+        Map result = new HashMap<String, List<Comment>>();
+        //返回
         if(comments.size() != 0){
-            return ResultUtils.onSuccess(JSON.toJSONString(comments));
+            return new ResponseEntity<ResultDTO>(ResultUtils.onSuccess(result),headers, HttpStatus.OK);
         }
         else{
-            return ResultUtils.onError("no comment");
+            throw new ResultException("no comments or error");
         }
     }
     //对一条微博评论
-    @PostMapping("/")
-    public ResultDTO sendAComment(@RequestBody CommentDTO commentDTO) throws Exception{
-        Comment comment = commentService.commentATweet(commentDTO);
+    @PostMapping
+    public ResponseEntity<ResultDTO> sendAComment(UriComponentsBuilder uriComponentsBuilder,@RequestBody CommentDTO commentDTO) throws Exception{
+        //包装header
+        HttpHeaders headers = ApplicationUtils.getHttpHeaders(uriComponentsBuilder,PATH);
+        headers.setAccessControlAllowCredentials(true);
+        headers.setAccessControlAllowOrigin("*");
 
+        Comment comment = commentService.commentATweet(commentDTO);
         Map result = new HashMap<String,Integer>();
-        result.put("CID",comment.getCid());
         //实际上不需要这么完整的数据，只需id即可
-        return ResultUtils.onSuccess(JSONObject.toJSONString(result));
+        result.put("CID",comment.getCid());
+        //返回
+        return new ResponseEntity<ResultDTO>(ResultUtils.onSuccess(result),headers,HttpStatus.OK);
     }
 
     //删除一条评论
-    @DeleteMapping("/{CID}")
-    public ResultDTO deleteATweet(@PathVariable(name = "CID") int CID)throws Exception{
+    @DeleteMapping(value = CIDPATH)
+    public ResponseEntity<ResultDTO> deleteATweet(UriComponentsBuilder uriComponentsBuilder,@PathVariable(name = "CID") int CID)throws Exception{
+        //包装header
+        HttpHeaders headers = ApplicationUtils.getHttpHeaders(uriComponentsBuilder,PATH+"/"+CID);
+        headers.setAccessControlAllowCredentials(true);
+        headers.setAccessControlAllowOrigin("*");
 
         commentService.deleteAComment(CID);
-        return ResultUtils.onSuccess();
+        //返回
+        return new ResponseEntity<ResultDTO>(ResultUtils.onSuccess(),headers,HttpStatus.OK);
     }
 
 
---
+++ src/main/java/com/bupt/weibo/dto/mapper/CommentMapper.java
@@ -0,0 +1,23 @@
+package com.bupt.weibo.dto.mapper;
+
+import com.bupt.weibo.dto.CommentDTO;
+import com.bupt.weibo.entity.Comment;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel ="spring")
+public interface CommentMapper {
+
+    Comment convertToEntity(CommentDTO CommentDto);
+
+    @InheritInverseConfiguration
+    CommentDTO convertToDto(Comment Comment);
+
+    List<Comment> convertToListEntity(List<CommentDTO> CommentDtoList);
+
+    @InheritInverseConfiguration
+    List<CommentDTO> conovertToListDto(List<Comment> CommentList);
+
+}
