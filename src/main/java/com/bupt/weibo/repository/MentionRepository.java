859291607459099dbcd4d2f18f098ad02e240dea
谭上鸥 <tanshangou@hotmail.com>
2018-07-16 15:47:44 +0800

    mention接口适配

--- src/main/java/com/bupt/weibo/repository/MentionRepository.java
+++ src/main/java/com/bupt/weibo/repository/MentionRepository.java
@@ -4,11 +4,15 @@ import com.bupt.weibo.entity.Mention;
 import com.bupt.weibo.entity.MentionPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @anthor tanshangou
  * @time 2018/7/7
  * @description
  */
 public interface MentionRepository extends JpaRepository<Mention,MentionPK> {
+
+    List<Mention> findAllByUid(String uid);
 
 }
--- src/main/java/com/bupt/weibo/service/impl/MentionServiceImpl.java
+++ src/main/java/com/bupt/weibo/service/impl/MentionServiceImpl.java
@@ -1,9 +1,39 @@
 package com.bupt.weibo.service.impl;
 
+import com.bupt.weibo.dto.MentionDTO;
+import com.bupt.weibo.dto.mapper.MentionMapper;
+import com.bupt.weibo.entity.Mention;
+import com.bupt.weibo.repository.MentionRepository;
+import com.bupt.weibo.service.MentionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @anthor tanshangou
  * @time 2018/7/13
  * @description
  */
-public class MentionServiceImpl {
+
+@Service
+public class MentionServiceImpl implements MentionService {
+    @Autowired
+    MentionRepository mentionRepository;
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    MentionMapper mentionMapper;
+
+    @Override
+    public List<Mention> getMentionsByUID(String UID) {
+
+        return mentionRepository.findAllByUid(UID);
+    }
+
+    @Override
+    public void addMention(MentionDTO mentionDTO) {
+
+        Mention mention = mentionMapper.convertToEntity(mentionDTO);
+        mentionRepository.save(mention);
+    }
 }
