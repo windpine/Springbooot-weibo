9fbd1f22e117e3e81a9cf8a81bb8642e11d3f645
谭上鸥 <tanshangou@hotmail.com>
2018-07-19 09:20:18 +0800

    websocket测试

--- src/main/java/com/bupt/weibo/entity/enums/ErrorCode.java
+++ src/main/java/com/bupt/weibo/entity/enums/ErrorCode.java
@@ -13,7 +13,7 @@ public enum  ErrorCode {
     NOTAUTHC(40100,"not authc"),
     NOTAUTHZ(40101,"not authz"),
     USERNAMEORPASSWORD(40401,"用户名或密码不正确"),
-    USEREXIST(40402,"用户名已存在");
+    USEREXIST(40402,"用户名已存在"),
     NOFOLLOWORFOLLOWER(40403,"no follows or followers");
 
     private final int code;
